acbce1f1a43c04045dd17ed7cb5e2c09f8ed6a78
Ahmad Herzallah <ahmedpalhr0595@gmail.com>
2022-01-07 10:18:50 +0200

    .

---
+++ server/controllers/student.js
@@ -0,0 +1,21 @@
+import student from "../models/student.js";
+
+export const getStudent = (req, res) => {
+  student
+    .find()
+    .then((student) => {
+      return res.json(student);
+    })
+    .catch((err) => res.status(400).json(err.message));
+};
+
+export const addStudent = (req, res) => {
+  const newStudent = new student(req.body);
+  newStudent
+    .save()
+    .then((student) => {
+      return res.json(student);
+    })
+    .catch((err) => res.status(400).json(err.message));
+};
+Insomania;
